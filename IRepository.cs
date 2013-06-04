dd1b896194838660fb6e74e895b7e39d92fe028c
Simon Harry <sharryster@gmail.com>
2013-06-04 18:33:48 +0200

    first

---
+++ IRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Interface for all repositories
+/// </summary>
+namespace sharry.blog.repository
+{
+    public interface IRepository<T> : IDisposable
+    {
+       T getElementById(int id);
+
+       List<T> getAllElements();
+
+       int insertElement(T t);
+
+       int updateElement(T t);
+    }
+}
